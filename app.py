479c5f681aeb207a6aa573e0b0b1ec76ea586d0a
jose-humberto-07 <jhpnascimento96@gmail.com>
2024-10-30 22:07:26 -0300

    project created

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def main():
+    st.title("Hello Word, testando pacote novo em python pra me desenvovler como engenheiro de dados")
+
+if __name__=="__main__":
+    main()
